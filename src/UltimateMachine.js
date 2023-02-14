e49e5d28516cc71e8be215e589351306f322f6d3
Cristian Florea <79513347+FloreaCT@users.noreply.github.com>
2023-02-14 10:59:25 +0000

    Learned about returns using turnary operator

--- src/UltimateMachine.js
+++ src/UltimateMachine.js
@@ -1,39 +1,21 @@
 import React, {useState} from 'react';
 
-const OnMessage = () => <span>The Machine is ON!</span>
-
-const OffMessage = () => <span>The Machine is OFF!</span>
-
-const OnOff = ({isOn})=> {
-    if(isOn){
-        return <OnMessage/>
-    } else {
-        return <OffMessage />
-    }
-}
+const ErrorMessage = ({showError}) => showError ? <span>Oh no, you have an error!</span> : null;
 
 const UltimateMachine = () => {
-    const [isOn, setIsOn] = useState(false);
+    const [showError, setShowError] = useState(false);
 
     const onClickHandler = () => {
-        setIsOn(i => !i);
-    }
-
-    const getMessage = () => {
-        if(isOn){
-            return <OnMessage/>
-        } else {
-            return <OffMessage/>
-        }
+        setShowError(i => !i);
     }
 
     return <section>
         <h1>The Ultimate Machine</h1>
-        {getMessage()}
-        <button type="button" 
+        <ErrorMessage showError={showError} />
+                <button type="button" 
         onClick={onClickHandler}  
-        aria-pressed={isOn}>
-            On/Off
+        aria-pressed={showError}>
+            Toggle Error
         </button>
     </section>
 };
